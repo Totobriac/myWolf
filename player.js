de66bed4d18a8df75b6096e589d2fdac7d607f67
Toto Briac <totobriac@gmail.com>
2021-12-28 02:45:44 +0100

    collision issues

--- player.js
+++ player.js
@@ -1,11 +1,12 @@
 class Player {
-  constructor(x, y, ctx) {
+  constructor(x, y, ctx,map) {
     this.x = x;
     this.y = y;
+    this.map = map;
     this.dir = 0;
     this.rot = 0;
     this.speed = 0;
-    this.moveSpeed = 0.18;
+    this.moveSpeed = 0.9;
     this.rotSpeed = 6 * Math.PI / 180;
     this.ctx = ctx;
     this.miniMapScale = 8;
@@ -16,25 +17,29 @@ class Player {
   move() {
     var moveStep = this.speed * this.moveSpeed;
     this.rot += this.dir * this.rotSpeed;
+
     var newX = this.x + Math.cos(this.rot) * moveStep;
     var newY = this.y + Math.sin(this.rot) * moveStep;
-    this.x = newX;
-    this.y = newY;
+
+    var canMove = this.collisonChecker(newX, newY);
+
+    if (canMove === true) {
+      this.x = newX;
+      this.y = newY;
+    } else return
+
+
   }
   draw() {
-
     this.move();
-
     var xLine = this.x + Math.cos(this.rot) * 20;
     var yLine = this.y + Math.sin(this.rot) * 20;
-
 
     this.ctx.beginPath();
     this.ctx.moveTo(this.x + 4 + this.xOffset, this.y + 4 + this.yOffset);
     this.ctx.lineTo(xLine + 4 + this.xOffset, yLine + 4 + this.yOffset);
     this.ctx.strokeStyle = "red";
     this.ctx.stroke();
-
 
     this.ctx.fillStyle = "black";
     this.ctx.fillRect(
@@ -43,6 +48,28 @@ class Player {
       this.miniMapScale,
       this.miniMapScale);
   }
+  collisonChecker(x, y) {
+    var angle = normalizeAngle(this.rot);
+
+    var direction;
+    var nextTile;
+
+    angle >= 0 && angle < Math.PI ? direction = "down" : direction = "up";
+
+    direction === "up" ? nextTile = this.map.bluePrint[Math.floor(y/8)][Math.floor(x/8)] : nextTile = this.map.bluePrint[Math.ceil(y/8)][Math.ceil(x/8)];;
+
+    var canGo;
+    nextTile != 0 ? canGo = false : canGo = true;
+    return canGo
+  }
+}
+
+function normalizeAngle(angle) {
+  angle = angle % (2 * Math.PI);
+  if (angle < 0) {
+    angle = (2 * Math.PI) + angle;
+  }
+  return angle;
 }
 
 export {
--- script.js
+++ script.js
@@ -17,14 +17,14 @@ canvas.height = 400;
 canvas.width = 1200;
 
 var map = new Map(level, ctx);
-var player = new Player(16, 16, ctx);
+var player = new Player(16, 16, ctx,map);
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   map.draw();
   setUpControls(player);
-  player.draw();
+  player.draw(map);
 
   requestAnimationFrame(animate)
 }
